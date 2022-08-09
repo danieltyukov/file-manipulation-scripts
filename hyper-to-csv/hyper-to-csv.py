e20e853ec9d8988fd2317a039cd3cabf41dd63f5
Mariano <ciminomariano@gmail.com>
2022-08-09 14:14:16 -0300

    Adding Output folder, adding path OS for Filename,Adding new Hyperfile,

--- hyper-to-csv/hyper-to-csv.py
+++ hyper-to-csv/hyper-to-csv.py
@@ -1,9 +1,14 @@
+import os
+
 import pantab
 from tableauhyperapi import TableName
 
 table_name = TableName("Extract", "Extract")
-hyper_name = "federated_0dppysj0xiytrz1fcj8v40"
-file_name = "federated_0dppysj0xiytrz1fcj8v40.hyper"
+hyper_name = "federated_0adm2280nevxa21fel48n0"
+file_name = "federated_0adm2280nevxa21fel48n0.hyper"
+
+path = os.getcwd()
+file_name = path + "\\"+"federated_0adm2280nevxa21fel48n0.hyper"
 
 df = pantab.frame_from_hyper(file_name, table=table_name)
 df.to_csv("output/{}.csv".format(hyper_name))
