fd3d0149b1173f46128dfda64a325357a3a9a77c
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2022-08-28 01:51:38 +0200

    lambda update

--- lambdas/GUA-file-handler.py
+++ lambdas/GUA-file-handler.py
@@ -1,40 +1,69 @@
 import boto3
 import re
+import time
+import urllib
 
 def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
-
     source_bucket = event['Records'][0]['s3']['bucket']['name']
-    object_key = event['Records'][0]['s3']['object']['key']
+    print('Before Try --->  Source Bucket Name: (Name of the source Bucket)' + source_bucket)
+    object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    print('Before Try --->  Object key (The name of the file Upload): ' + object_key)
     file_type = object_key.split('.')[-1]
-    file_name = object_key.split('/')[-1]
-
-    file_name = re.sub("[!@#$%^&*()[]{};:,./<>?\|`~-=_+]", "", file_name)
-
-    print('Source bucket: ' + source_bucket)
-    print('Object key: ' + object_key)
-    print('File type: ' + file_type)
-    print('File name: ' + file_name)
-    
+    print('Before Try --->  File type: (CSV XLSX HYPER)' + file_type)
+    file_name = str(object_key.split('/')[-1])
+    print('Before Try ----> File name: (Single File Name) ' + file_name)
+    copy_source = {'Bucket': source_bucket, 'Key': object_key}
     try:
-
-        # if file xlsx copy file to excel folder, if file csv copy file to csv-main folder, if file hyper copy file to hyper folder
-        copy_source = {'Bucket': source_bucket, 'Key': object_key}
-        if file_type == 'xlsx':
-            s3.meta.client.copy(copy_source, source_bucket, 'excel/' + file_name)
-        elif file_type == 'csv':
-            s3.meta.client.copy(copy_source, source_bucket, 'csv-main/' + file_name)
+    
+        if file_type == 'csv':  
+            try:
+                s3.meta.client.copy(copy_source, source_bucket, 'csv-main/' + file_name)
+                s3.meta.client.copy(copy_source, source_bucket, 'archive/' + file_name)
+                print("The file was copied to csv-main")
+            except Exception as e:
+                s3.meta.client.copy(copy_source, source_bucket, 'unprocessed/' + file_name)
+                print("The file was not copied")
+            try:                        
+                s3.meta.client.delete_object(Bucket=source_bucket,Key=object_key)
+                print("The file was deleted")
+            except Exception as e:
+                print("The file was not deleted")
+        elif file_type == 'xlsx':
+            try:
+                s3.meta.client.copy(copy_source, source_bucket, 'excel/' + file_name)
+                s3.meta.client.copy(copy_source, source_bucket, 'archive/' + file_name)
+                print("The file was copied to excel")
+            except Exception as e:
+                s3.meta.client.copy(copy_source, source_bucket, 'unprocessed/' + file_name)
+                print("The file was not copied")
+            try:                        
+                s3.meta.client.delete_object(Bucket=source_bucket,Key=object_key)
+                print("The file was deleted")
+            except Exception as e:
+                print("The file was not deleted")
         elif file_type == 'hyper':
-            s3.meta.client.copy(copy_source, source_bucket, 'hyper/' + file_name)
+            try:
+                s3.meta.client.copy(copy_source, source_bucket, 'hyper/' + file_name)
+                s3.meta.client.copy(copy_source, source_bucket, 'archive/' + file_name)
+                print("The file was copied to hyper")
+            except Exception as e:
+                s3.meta.client.copy(copy_source, source_bucket, 'unprocessed/' + file_name)
+                print("The file was not copied")
+            try:                        
+                s3.meta.client.delete_object(Bucket=source_bucket,Key=object_key)
+                print("The file was deleted")
+            except Exception as e:
+                print("The file was not deleted")
         else:
             s3.meta.client.copy(copy_source, source_bucket, 'reject/' + file_name)
-
-        # copy file to archive folder
-        s3.meta.client.copy(copy_source, source_bucket, 'archive/' + file_name)
-
-        # delete file from source bucket
-        s3.meta.client.delete_object(Bucket=source_bucket, Key=object_key)
-        
+            s3.meta.client.copy(copy_source, source_bucket, 'archive/' + file_name)
+            try:                        
+                s3.meta.client.delete_object(Bucket=source_bucket,Key=object_key)
+                print("The file was deleted")
+            except Exception as e:
+                print("The file was not deleted")
+            
     except Exception as err:
         print ("Error -"+str(err))
