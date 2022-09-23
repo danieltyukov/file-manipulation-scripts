9676f3053575672ef0a907493eb9352119691959
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2022-09-23 03:17:12 +0200

    updates

--- lambdas/GUA-upload-redshift.py
+++ lambdas/GUA-upload-redshift.py
@@ -1,44 +1,64 @@
 import psycopg2
 import boto3
 import urllib
-
+import re
+import os
 
 def lambda_handler(event, context):
+    try:
 
-    # when CSV file lands in the bucket folder its name must be taken.
-    # the file name must be used to create a table in Redshift.
-    # the file must be copied to the Redshift database.
-    # the file must be deleted from the bucket.
-    # if the file cannot be copied to Redshift, it must be moved to the unproucessed folder.
+        passw = os.environ['pass']
+        aws_access_key_id = os.environ['aws_access_key_id']
+        aws_secret_access_key = os.environ['aws_secret_access_key']
 
-    # folder name: csv-main
+        # folder name: csv-main
+        s3 = boto3.resource('s3')
+        source_bucket = event['Records'][0]['s3']['bucket']['name']
 
-    s3 = boto3.resource('s3')
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
+        # get file name
+        object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+        file_name = str(object_key.split('/')[-1])
 
-    # get file name
-    object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
-    file_name = str(object_key.split('/')[-1])
+        create_table_statement = "CREATE TABLE IF NOT EXISTS " + file_name.split('.')[0] + " ("
+        with open('/tmp/' + file_name, 'wb') as data:
+            s3.Bucket(source_bucket).download_fileobj(object_key, data)
+        with open('/tmp/' + file_name, 'r') as data:
+            header = data.readline().split(',')
+            for i in range(len(header)):
+                header[i] = re.sub('[^a-zA-Z0-9 \n\.]', '', header[i])
+                header[i] = header[i].replace(' ', '_')
+                create_table_statement += header[i] + " varchar(100),"
 
-    # generate create table statement out of the csv file
-    create_table_statement = "CREATE TABLE IF NOT EXISTS " + file_name + " ("
-    with open('/tmp/' + file_name, 'wb') as data:
-        s3.Bucket(source_bucket).download_fileobj(object_key, data)
-    with open('/tmp/' + file_name, 'r') as data:
-        header = data.readline().split(',')
-        for column in header:
-            create_table_statement += column + " varchar(255),"
-        create_table_statement = create_table_statement[:-1] + ");"
-    
-    print(create_table_statement)
+            create_table_statement = create_table_statement[:-1] + ");"
 
-    # connect to Redshift
-    conn = psycopg2.connect(dbname='dev', host='redshift-cluster-1.cqjzjxqjxqjx.us-east-1.redshift.amazonaws.com', port='5439', user='awsuser', password='Password1')
-    cur = conn.cursor()
+        print("create table statement: ")
+        print(create_table_statement)
 
-    # create table in Redshift
-    cur.execute(create_table_statement)
-    conn.commit()
+        # connect to Redshift
         
+        conn = psycopg2.connect(dbname='dev', host='redshift-cluster-guardian.cqp1qbc3xi4l.ap-south-1.redshift.amazonaws.com', port='5439', user='awsuser', password=passw, options='-c search_path=ppo')
+        cur = conn.cursor()
 
-     
+        # create table in Redshift
+        cur.execute(create_table_statement)
+        conn.commit()
+
+        # fill in data to the table
+
+        # copy statement
+        copy_statement = "COPY " + file_name.split('.')[0] + " FROM 's3://" + source_bucket + "/" + "csv-main/" + file_name + "' CREDENTIALS 'aws_access_key_id=" + aws_access_key_id + ";aws_secret_access_key=" + aws_secret_access_key + "' DELIMITER ',' IGNOREHEADER 1 REGION 'ap-south-1';"
+        print("copy statement: ")
+        print(copy_statement)
+
+        # copy data to Redshift
+        cur.execute(copy_statement)
+        conn.commit()
+
+        # delete file from S3
+        s3.Object(source_bucket, object_key).delete()
+    
+    except Exception as e:
+        print(e)
+        s3.Object(source_bucket, 'unprocessed/' + file_name).copy_from(CopySource=source_bucket + '/' + object_key)
+        s3.Object(source_bucket, object_key).delete()
+        raise e
